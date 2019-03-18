5a8a6838c88e0c6a5eef1e5364f860f0df91e313
David Schuler <dtschule@ncsu.edu>
2019-03-18 13:42:16 -0400

    img loaded in directly to memory as np array

--- objectDetectionApp/detect-objects/funcmodule.py
+++ objectDetectionApp/detect-objects/funcmodule.py
@@ -148,10 +148,10 @@ def run_detection(PATH_TO_TEST_IMAGES_DIR):
 # Size, in inches, of the output images.
     IMAGE_SIZE = (12, 8)
     #image = Image.open(image_path)
-    image = img
+    image_np = img
 # the array based representation of the image will be used later in order to prepare the
 # result image with boxes and labels on it.
-    image_np = load_image_into_numpy_array(image)
+    #image_np = load_image_into_numpy_array(image)
 # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
 # Actual detection.
